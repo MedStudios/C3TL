1178fedfe3941d398cca5d9f5093466c07e552dd
谭视怀 Tan Shihuai <tsh20011021@outlook.com>
2020-06-05 17:53:01 +0800

    Add 'iterator.h' redirected to STL <iterator>

---
+++ include/iterator.h
@@ -0,0 +1,15 @@
+#ifndef _C3TL_ITERATOR_H_
+#define _C3TL_ITERATOR_H_
+
+#include <iterator>
+
+namespace c3
+{
+
+// 未实现部分的重定向
+template <typename T>
+using reverse_iterator = std::reverse_iterator<T>;
+
+} // namespace c3
+
+#endif // _C3TL_ITERATOR_H_
